41ac67b96a3374b33f6ad0c4ba837b6db2aac919
Julien Durillon <julien.durillon@kan-op.com>
2010-09-26 18:07:14 +0200

    Client propre

--- src/hackervaillant/net/Client.java
+++ src/hackervaillant/net/Client.java
@@ -1,12 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package hackervaillant.net;
-
-import java.net.DatagramSocket;
-import java.net.Socket;
 
 /**
  *
