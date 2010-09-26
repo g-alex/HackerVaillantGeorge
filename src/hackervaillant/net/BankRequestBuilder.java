e620ca997a0b07737e1637dab0f5f3dbce94568e
Julien Durillon <julien.durillon@kan-op.com>
2010-09-26 17:52:53 +0200

    début gestion requête

---
+++ src/hackervaillant/net/BankRequestBuilder.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hackervaillant.net;
+
+/**
+ *
+ * @author judu
+ */
+public class BankRequestBuilder {
+
+}
---
+++ src/hackervaillant/net/Client.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hackervaillant.net;
+
+/**
+ *
+ * @author judu
+ */
+public class Client {
+
+   public enum Host {
+      BANK, PEOPLE;
+   }
+
+
+   public enum Proto {
+      TCP, UDP;
+   }
+
+   public Client() {
+   }
+
+
+
+   
+
+}
